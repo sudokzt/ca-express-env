4a401d22711c3e0a1475cca46d3e040dbcb6ca99
kaaazu <wato9902@gmail.com>
2021-02-06 23:40:33 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ const app = Express();
 
 app.use(cors());
 
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
 // app.set('trust proxy', true);
 
 app.use(
